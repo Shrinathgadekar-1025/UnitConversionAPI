9a22beb6907ba4c9c25de18daa53add31515276f
Shrinath Gadekar <shrinath658@gmail.com>
2026-06-13 22:39:04 +0530

    Add conversion request and response models

---
+++ UnitConversionAPI/Models/ConversionRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UnitConversionAPI.Models
+{
+    public class ConversionRequest
+    {
+        [Required]
+        public string FromUnit { get; set; } = string.Empty;
+
+        [Required]
+        public string ToUnit { get; set; } = string.Empty;
+
+        [Range(double.MinValue, double.MaxValue)]
+        public double Value { get; set; }
+    }
+}
---
+++ UnitConversionAPI/Models/ConversionResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace UnitConversionAPI.Models
+{
+    public class ConversionResponse
+    {
+        public string FromUnit { get; set; } = string.Empty;
+        public string ToUnit { get; set; } = string.Empty;
+        public double InputValue { get; set; }
+        public double Result { get; set; }
+        public string Category { get; set; } = string.Empty;
+    }
+}
